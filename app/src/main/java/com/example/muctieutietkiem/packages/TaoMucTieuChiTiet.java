e5cb4a337aa480bfa68dd858be5faf148f69e7db
tamphan1506 <tamptm19411@st.uel.edu.vn>
2021-11-20 03:05:43 +0700

    Tạo mục tiêu TK

---
+++ app/src/main/java/com/example/muctieutietkiem/packages/TaoMucTieuChiTiet.java
@@ -0,0 +1,16 @@
+package com.example.muctieutietkiem.packages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartmanagertwo.R;
+
+public class TaoMucTieuChiTiet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tao_muc_tieu_chi_tiet);
+    }
+}
