97dc8d173ef1005e1423d69c5bdf210e18c64f7a
diemntt19411 <diemntt19411@st.uel.edu.vn>
2021-11-23 21:35:07 +0700

    Updates

--- app/src/main/java/com/example/smartmanagertwo/KetNoiTinTuc.java
+++ app/src/main/java/com/example/smartmanagertwo/KetNoiTinTuc.java
@@ -8,6 +8,6 @@ public class KetNoiTinTuc extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ketnoi_tintuc);
+        setContentView(R.layout.activity_ketnoi_tintuc_chitiet);
     }
 }
