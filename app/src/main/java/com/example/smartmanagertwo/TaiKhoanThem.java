66160e7a7a10fc47529496a16ae5e3f857b3e2e4
diemntt19411 <diemntt19411@st.uel.edu.vn>
2021-12-05 21:12:11 +0700

    ThemTaiKhoan

--- app/src/main/java/com/example/smartmanagertwo/TaiKhoanThem.java
+++ app/src/main/java/com/example/smartmanagertwo/TaiKhoanThem.java
@@ -1,10 +1,25 @@
 package com.example.smartmanagertwo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class TaiKhoanThem extends AppCompatActivity {
 
@@ -19,5 +34,61 @@ public class TaiKhoanThem extends AppCompatActivity {
 
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.chu_dao)));
         getSupportActionBar().setTitle("Thêm tài khoản");
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View v = getCurrentFocus();
+        if (v != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) &&
+                v instanceof EditText &&
+                !v.getClass().getName().startsWith("android.webkit.")) {
+            int[] sourceCoordinates = new int[2];
+            v.getLocationOnScreen(sourceCoordinates);
+            float x = ev.getRawX() + v.getLeft() - sourceCoordinates[0];
+            float y = ev.getRawY() + v.getTop() - sourceCoordinates[1];
+            if (x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom()) {
+                hideKeyboard(this);
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void hideKeyboard(Activity activity) {
+        if (activity != null && activity.getWindow() != null) {
+            activity.getWindow().getDecorView();
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+            }
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+//            case R.id.mnLich:
+//                Calendar calendarDate= Calendar.getInstance();
+//                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//                DatePickerDialog.OnDateSetListener callBack= new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+//                        calendarDate.set(Calendar.YEAR,i);
+//                        calendarDate.set(Calendar.MONTH,i1);
+//                        calendarDate.set(Calendar.DAY_OF_MONTH,i2);
+//                    }
+//                };
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(TaiKhoanThem.this,callBack,
+//                        calendarDate.get(Calendar.YEAR),
+//                        calendarDate.get(Calendar.MONTH),
+//                        calendarDate.get(Calendar.DAY_OF_MONTH));
+//                datePickerDialog.show();
+//                break;
+            default:break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
