6c259d26d8362c8e0203beb8338a235966d6e210
thamnth <thamnth19411@st.uel.edu.vn>
2021-12-13 20:27:26 +0700

    Update

---
+++ app/src/main/java/com/example/smartmanagertwo/MatKhauOff.java
@@ -0,0 +1,14 @@
+package com.example.smartmanagertwo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MatKhauOff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cai_dat_mat_khau_tat);
+    }
+}
