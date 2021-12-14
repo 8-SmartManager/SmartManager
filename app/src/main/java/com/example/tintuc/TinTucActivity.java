7888926211874dfa0e1ddf661fdd618ded379606
thamnth <thamnth19411@st.uel.edu.vn>
2021-12-14 20:36:04 +0700

    update

--- app/src/main/java/com/example/tintuc/TinTucActivity.java
+++ app/src/main/java/com/example/tintuc/TinTucActivity.java
@@ -1,61 +1,60 @@
 package com.example.tintuc;
 
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smartmanagertwo.R;
+import com.example.tintuc.KetNoiTinTucChiTiet;
+import com.example.tintuc.TinTucAdapter;
+import com.example.tintuc.TinTucData;
 
 import java.util.ArrayList;
 
-public class TinTucActivity extends AppCompatActivity {
+public class TinTucActivity extends Fragment {
     ListView lvTinTuc;
     TinTucAdapter adapter;
     ArrayList <TinTucData> news;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tintuc);
 
-        Drawable drawable=getResources().getDrawable(R.drawable.ic_menu);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(drawable);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.chu_dao)));
-        getSupportActionBar().setTitle("Tin Tức");
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.activity_tintuc,container,false);
 
-        linkViews();
+        lvTinTuc=root.findViewById(R.id.lvTinTuc);
         iniData();
         loadData();
         addEvent();
+        return root;
     }
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void addEvent() {
         lvTinTuc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent=new Intent(TinTucActivity.this,KetNoiTinTucChiTiet.class);
+                Intent intent=new Intent(getActivity(), KetNoiTinTucChiTiet.class);
                 startActivity(intent);
             }
         });
     }
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void loadData() {
-        adapter=new TinTucAdapter(getBaseContext(),R.layout.item_tintuc,iniData());
+        adapter=new TinTucAdapter(getContext(),R.layout.item_tintuc,iniData());
         lvTinTuc.setAdapter(adapter);
     }
-
-
-
-    private void linkViews() {
-        lvTinTuc=findViewById(R.id.lvTinTuc);
-    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private ArrayList<TinTucData> iniData() {
         news= new ArrayList<>();
         news.add(new TinTucData("Personal Finance Tips for 2020: “The Simple Path to Wealth","Find your road map to financial independence and a rich life through simple awareness.",R.drawable.anh1));
@@ -65,5 +64,4 @@ public class TinTucActivity extends AppCompatActivity {
 
         return news;
     }
-
 }
