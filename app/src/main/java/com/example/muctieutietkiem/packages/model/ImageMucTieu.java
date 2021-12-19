53ae0a85710046d7ed1076fe068c4d73a041b542
ngankim-1550 <nganntk19411@st.uel.edu.vn>
2021-12-19 10:50:26 +0700

    image mục tiêu

---
+++ app/src/main/java/com/example/muctieutietkiem/packages/model/ImageMucTieu.java
@@ -0,0 +1,17 @@
+package com.example.muctieutietkiem.packages.model;
+
+public class ImageMucTieu {
+    int ImageId;
+
+    public int getImageId() {
+        return ImageId;
+    }
+
+    public void setImageId(int imageId) {
+        ImageId = imageId;
+    }
+
+    public ImageMucTieu(int imageId) {
+        ImageId = imageId;
+    }
+}
